e1d1ec634688988061ef0caf5189459d61eab186
Ryan Wells <rawells14@gmail.com>
2014-10-28 15:08:57 -0500

    Added mainapp

--- js/mainapp.js
+++ js/mainapp.js
@@ -1,4 +1,18 @@
-$(document).ready(function(){
-	
-});		
+$(document).ready(function () {
+    
+});
 
+
+function changeGpaElement(input){
+    
+}
+
+
+
+function getGpa(input) {
+    var finalGpa = 6.0;
+    
+    
+   
+    return finalGpa;
+}
