074d2afa8f4c3129a3432ee1e1fc240220f3e8b8
Ryan Wells <rawells14@gmail.com>
2014-10-29 10:25:51 -0500

    getGpa Method fixed and Animation of the Gpa Bar

--- js/mainapp.js
+++ js/mainapp.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
     $("#buttonWrapper").click(function () {
         
-        $("#progbar").animate({ width: "100%" },300);
+        $("#progbar").animate({width: (getGpa(70, 6.0) / 6.0)*100 + "%"}, 300);
+        
     });
 });
 
@@ -15,12 +16,10 @@ function changeGpaElement(input) {
 
 
 
-function getGpa(input) {
-    input = 100 - input;
-    var finalGpa = 6.0;
-
-
-
-
+function getGpa(grade, scale) {
+    var finalGpa = scale;
+    grade = 100-grade;
+    grade /= 10;
+    finalGpa = finalGpa-grade;
     return finalGpa;
 }
