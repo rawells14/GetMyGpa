a63130612d76611c196452da95f6f8ccebd1ae12
Ryan Wells <rawells14@gmail.com>
2014-10-29 17:05:55 -0500

    Added Averaging feature and enhancements to calc.

--- js/mainapp.js
+++ js/mainapp.js
@@ -1,25 +1,33 @@
 $(document).ready(function () {
     $("#buttonWrapper").click(function () {
-        
-        $("#progbar").animate({width: (getGpa(70, 6.0) / 6.0)*100 + "%"}, 300);
-        
+
+        clearElements();
+        changeGpaElement();
+
     });
 });
+var amtOfGpas = 0;
+var avgGpa = 0;
+function clearElements() {
 
 
-function changeGpaElement(input) {
-
-    if (input >= 5.0) {
-
-    }
 }
+function changeGpaElement() {
+    var grade = $("#input1").val();
+    var scale = $("#scale").val();
+    var gpa = getGpa(grade, scale);
 
+    amtOfGpas++;
+    avgGpa = ((avgGpa * (amtOfGpas - 1)) + gpa) / amtOfGpas;
+    console.log(avgGpa);
 
-
+    $("#progbar").animate({width: (avgGpa / 6) * 100 + "%"}, 450);
+    $("#hi").val("Testing");
+}
 function getGpa(grade, scale) {
     var finalGpa = scale;
-    grade = 100-grade;
+    grade = 100 - grade;
     grade /= 10;
-    finalGpa = finalGpa-grade;
+    finalGpa = finalGpa - grade;
     return finalGpa;
 }
