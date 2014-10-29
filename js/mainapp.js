51a50a85686fd654a5715415012721e24896e242
Ryan Wells <rawells14@gmail.com>
2014-10-29 10:09:31 -0500

    Added Gpa Scale Bar and animation

--- js/mainapp.js
+++ js/mainapp.js
@@ -1,20 +1,26 @@
 $(document).ready(function () {
-    
+    $("#buttonWrapper").click(function () {
+        
+        $("#progbar").animate({ width: "100%" },300);
+    });
 });
 
 
-function changeGpaElement(input){
-    if (input>=5.0) {
-        
+function changeGpaElement(input) {
+
+    if (input >= 5.0) {
+
     }
 }
 
 
 
 function getGpa(input) {
+    input = 100 - input;
     var finalGpa = 6.0;
-    
-    
-   
+
+
+
+
     return finalGpa;
 }
